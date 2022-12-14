0471ee61cd231b29464c68d8fdae0e9700033432
tusharparmar10050 <tusharparmar10050@gmail.com>
2022-12-14 19:00:45 +0530

    first commit

---
+++ room/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.rooms, name='rooms'),
+    path('<slug:slug>/', views.room, name='room'),
+   
+]
